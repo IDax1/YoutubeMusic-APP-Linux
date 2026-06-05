39d6dbb34389f532747aa8df5a3c92cd9d3c58fa
Max Carrizo <151425667+IDax1@users.noreply.github.com>
2026-06-05 12:07:50 -0400

    Update ytmusic.py

--- ytmusic.py
+++ ytmusic.py
@@ -12,31 +12,31 @@ class YouTubeMusicApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Carpeta donde está el script
+        
         base_dir = Path(__file__).resolve().parent
 
-        # Icono de la aplicación
+        #Icon
         icon_path = base_dir / "icon.png"
 
         self.setWindowTitle("YouTube Music")
         self.setWindowIcon(QIcon(str(icon_path)))
         self.resize(1400, 900)
 
-        # Carpeta para guardar sesión, cookies y datos
+        
         storage_path = Path.home() / ".config" / "YouTubeMusic"
         storage_path.mkdir(parents=True, exist_ok=True)
 
-        # Perfil persistente
+        
         profile = QWebEngineProfile("YouTubeMusic", self)
         profile.setPersistentStoragePath(str(storage_path))
         profile.setPersistentCookiesPolicy(
             QWebEngineProfile.PersistentCookiesPolicy.ForcePersistentCookies
         )
 
-        # Página web con perfil persistente
+        
         page = QWebEnginePage(profile, self)
 
-        # Navegador
+        
         self.browser = QWebEngineView()
         self.browser.setPage(page)
         self.browser.setUrl(QUrl("https://music.youtube.com"))
